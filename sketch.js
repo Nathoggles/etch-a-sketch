bbb74a7683153eec4e212fc8a44da8dadf399eeb
Nathoggles <mawricus@gmail.com>
2024-08-26 22:04:49 +0200

    implement colouring divs black

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ const containerInner = document.querySelector("#containerInner");
 function populateDivs(crossANumber) {
     console.log(crossANumber);
     removeAllChildNodes(containerInner);
-    for (let i = 1; i < crossANumber ** 2 * 2; i++) {
+    for (let i = 1; i < crossANumber ** 2 * 2 + 1; i++) {
         const div = document.createElement("div");
         div.setAttribute("class", "divs");
         div.style.flexBasis = `${100 / (crossANumber * 2)}%`;
@@ -22,5 +22,17 @@ function removeAllChildNodes(parent) {
 
 populateDivs(16);
 
+const divs = document.querySelectorAll(".divs");
 
-//add repopulation if resize?
+divs.forEach((div) => { 
+    div.addEventListener("pointerover", (event) => { event.target.style.background = "black";
+    })
+});
+
+
+
+
+
+/*click.forEach((button) => {
+    button.addEventListener("click", () => {*/
+//why do you need +1? ask later
