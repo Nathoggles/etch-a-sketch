4a778b9c1198c2961d379632a652e60defa2aa50
Nathoggles <mawricus@gmail.com>
2024-08-27 05:19:47 +0200

    start range slider from scratch

--- sketch.js
+++ sketch.js
@@ -33,14 +33,6 @@ divs.forEach((div) => {
 
 
 
-let slider = document.getElementById("myRange");
-let output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
-};
 
 
 
