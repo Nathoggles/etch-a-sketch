7650f6c545fdb1bba6654a78f1416c85e406d412
Nathoggles <mawricus@gmail.com>
2024-08-27 03:58:01 +0200

    attempt to implement slider

--- sketch.js
+++ sketch.js
@@ -33,6 +33,15 @@ divs.forEach((div) => {
 
 
 
+let slider = document.getElementById("myRange");
+let output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+};
+
 
 
 /*click.forEach((button) => {
