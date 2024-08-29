1ca2daeb8b8d5d2ed8bd352a598961a7a2820156
Nathoggles <mawricus@gmail.com>
2024-08-30 01:47:42 +0200

    make working toggles and half-working rgb generator

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 const containerInner = document.querySelector("#containerInner");
+const black = document.querySelector ("#black");
+const rgb = document.querySelector ("#rgb");
+const white = document.querySelector ("#white");
 
 
+let randomRgb; 
+let divsColor = "black";
 
 
 function populateDivs(crossANumber) {
-    console.log(crossANumber);
     removeAllChildNodes(containerInner);
     for (let i = 1; i < crossANumber ** 2 * 2 + 1; i++) {
         const div = document.createElement("div");
@@ -21,32 +25,29 @@ function removeAllChildNodes(parent) {
 };  
 
 populateDivs(16);
-
 const divs = document.querySelectorAll(".divs");
-let divsColor = "black";
+
+
+
+black.addEventListener("click", () => {
+    divsColor = "black";
+    console.log(divsColor);
+});
+rgb.addEventListener("click", () => {
+    divsColor = randomizeRGB();
+    console.log (divsColor);
+});
+white.addEventListener("click", () => {
+    divsColor = "white";
+});
+
 
 divs.forEach((div) => { 
-    div.addEventListener("pointerover", (event) => { event.target.style.background = divsColor;
+    div.addEventListener("pointerover", (event) => {event.target.style.background = divsColor;  
+        console.log(event.target);
     })
 });
 
-
-
-const sliderEl = document.querySelector("#myRange");
-const sliderValue = document.querySelector(".value");
-
-
-
-sliderEl.addEventListener("input", (event) => {
-    
-    const tempSliderValue = event.target.value;
-    sliderValue.textContent = tempSliderValue;
-    populateDivs(tempSliderValue);
-
-    const progress = (tempSliderValue / sliderEl.max) * 100;
-    sliderEl.style.background = `linear-gradient(to right, rgb(54, 31, 11) ${progress}%, rgb(122, 85, 50) ${progress}%)`; 
-   
-});
 
 
 function randomNumber() {
@@ -54,16 +55,30 @@ function randomNumber() {
     const maxFloored = Math.floor(256);
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
   }
-let randomRgb; 
+
+
 function randomizeRGB(){
- let randomRgb = "rgb(" + randomNumber() + ",&nbsp" + randomNumber() + ",&nbsp" + randomNumber() + ")";
- randomRgb.toString;
- console.log(randomRgb);
-}
-randomizeRGB();
+    let randomRgb = "rgb(" + randomNumber() + ", " + randomNumber() + ", " + randomNumber() + ")";
+    return randomRgb;
+    }
 
 
 
+
+const sliderEl = document.querySelector("#myRange");
+const sliderValue = document.querySelector(".value");
+
+
+    sliderEl.addEventListener("input", (event) => {
+        
+        const tempSliderValue = event.target.value;
+        sliderValue.textContent = tempSliderValue;
+        populateDivs(tempSliderValue);
+
+        const progress = (tempSliderValue / sliderEl.max) * 100;
+        sliderEl.style.background = `linear-gradient(to right, rgb(54, 31, 11) ${progress}%, rgb(122, 85, 50) ${progress}%)`; 
+    
+    });
 
 
 /*click.forEach((button) => {
